38f47d2f2fe72399410609fd0fdc09e88aa61ebd
mark <markrunkle67@gmail.com>
2025-04-10 14:10:43 -0500

    init

--- motionDetector.py
+++ motionDetector.py
@@ -14,26 +14,29 @@ window = tk.Tk()
 window.title("Motion Detection System")
 window.geometry("400x400")
 
-status_label = tk.Label(window, text="Status: Idle", font=("Arial", 20), background="lightgreen")
+status_label = tk.Label(window, text="Status: Idle", font=("Engravers MT", 20), background="lightgreen")
 status_label.pack(pady=20)
 
 
 def arm_system():
+    print("ARMED")
     global armed
     armed = True
-    status_label.config(text="Status: Armed", font=("Arial", 20), background="lightblue")
+    status_label.config(text="Status: Armed", font=("Engravers MT", 20), background="lightblue")
 
 
 
 def disarm_system():
+    print("DISARMED")
     global armed
     armed = False
-    status_label.config(text="Status: Idle", font=("Arial", 20), background="lightgreen")
+    status_label.config(text="Status: Idle", font=("Engravers MT", 20), background="lightgreen")
     led.off()
     pygame.mixer.music.stop()
 
 def alert():
-    status_label.config(text="MOTION DETECTED", font=("Arial", 20), background="red")
+    print("ALERTED")
+    status_label.config(text="MOTION DETECTED", font=("Engravers MT", 20), background="red")
     led.on()
     if not pygame.mixer.music.get_busy():
         pygame.mixer.music.play()
@@ -47,9 +50,9 @@ def check_status():
         window.after(3000, disarm_system)
     window.after(500, check_status)
 
-arm_button = tk.Button(window, text="Arm System", command=arm_system, font=("Arial", 20))
+arm_button = tk.Button(window, text="Arm System", command=arm_system, font=("Engravers MT", 20))
 arm_button.pack(pady=10)
-disarm_button = tk.Button(window, text="Disarm System", command=disarm_system, font=("Arial", 20))
+disarm_button = tk.Button(window, text="Disarm System", command=disarm_system, font=("Engravers MT", 20))
 disarm_button.pack(pady=10)
 
 check_status()
